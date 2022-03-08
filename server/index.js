1b14b57dde57b8ecf7e9d6800824fa8c23881a46
Vishwaraj-Rathore <vishwaraj3003@gmail.com>
2022-03-08 18:19:07 +0530

    trying out git

--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+console.log("hi");
+
 app.use("/posts", postRoutes);
 
 const PORT = process.env.PORT || 5000;
