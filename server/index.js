4705f7733f35db9a1a3cf8facd1113f0a6b82b96
Vishwaraj-Rathore <vishwaraj3003@gmail.com>
2022-03-14 10:18:29 +0530

    Added Login Functionality

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 
 import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 
 const app = express();
 dotenv.config();
@@ -13,9 +14,8 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-console.log("hi");
-
 app.use("/posts", postRoutes);
+app.use("/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
